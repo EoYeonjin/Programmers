aa7e6c2d8437d0cb55619d76fdc2d0bc0499a6dc
EoYeonjin <wks1211@gmail.com>
2024-10-21 11:22:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();
+        System.out.print(A+B+C);
+    }
+}
