b94df3d81201945c61a195f199639e4ff155ebff
EoYeonjin <wks1211@gmail.com>
2024-10-21 15:07:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.time.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws Exception {
+        int a = System.in.read();
+        System.out.print(a);
+    }
+}
