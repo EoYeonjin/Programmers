f79a476c22834432cc0e6592fa0bdf3d9683ac38
EoYeonjin <wks1211@gmail.com>
2024-10-25 11:22:42 +0900

    [Bronze V] Title: 웰컴, Time: 104 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .\n");
+        sb.append("|  | _ | _. _ ._ _  _\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.\n");
+        
+        System.out.print(sb);
+	}
+}
