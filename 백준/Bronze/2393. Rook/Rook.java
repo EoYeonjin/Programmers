f1733beaad6c489673527dd57b8a1f6aae00697a
EoYeonjin <wks1211@gmail.com>
2024-10-25 10:20:15 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("  ___  ___  ___\n");
+        sb.append("  | |__| |__| |\n");
+        sb.append("  |           |\n");
+        sb.append("   \\_________/\n");
+        sb.append("    \\_______/\n");
+        sb.append("     |     |\n");
+        sb.append("     |     |\n");
+        sb.append("     |     |\n");
+        sb.append("     |     |\n");
+        sb.append("     |_____|\n");
+        sb.append("  __/       \\__\n");
+        sb.append(" /             \\\n");
+        sb.append("/_______________\\\n");
+        
+        System.out.print(sb);
+	}
+}
