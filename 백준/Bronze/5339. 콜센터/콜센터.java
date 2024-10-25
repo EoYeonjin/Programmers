9f872ea7f5e9fb91b3720d2b9b17e28ac4831b67
EoYeonjin <wks1211@gmail.com>
2024-10-25 11:38:13 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("     /~\\\n");
+        sb.append("    ( oo|\n");
+        sb.append("    _\\=/_\n");
+        sb.append("   /  _  \\\n");
+        sb.append("  //|/.\\|\\\\\n");
+        sb.append(" ||  \\ /  ||\n");
+        sb.append("============\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        
+        System.out.print(sb);
+	}
+}
