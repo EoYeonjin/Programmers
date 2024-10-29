ffc91ce37ec413b417fcfb357f033e69da7bac60
EoYeonjin <wks1211@gmail.com>
2024-10-29 09:52:38 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 96 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("고려대학교").append("\n");
+
+        System.out.print(sb);
+	}
+}
