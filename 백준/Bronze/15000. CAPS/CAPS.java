782a97a2db5efd89330dc4f01d295224e7db4cf2
EoYeonjin <wks1211@gmail.com>
2024-10-30 15:23:15 +0900

    [Bronze V] Title: CAPS, Time: 280 ms, Memory: 23012 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String input = br.readLine();
+
+        sb.append(input.toUpperCase());
+        System.out.print(sb);
+        
+	}
+}
