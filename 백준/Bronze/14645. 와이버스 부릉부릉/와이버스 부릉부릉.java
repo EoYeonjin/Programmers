ca86cb5a08abbefac2f058dd6187e967483c788a
EoYeonjin <wks1211@gmail.com>
2024-10-30 15:01:01 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 88 ms, Memory: 13820 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("비와이").append("\n");
+
+        System.out.print(sb);
+	}
+}
