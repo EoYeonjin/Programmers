f2c2d693a2f041d8919d487d175aee725e7edbe7
EoYeonjin <wks1211@gmail.com>
2024-10-30 16:58:50 +0900

    [Bronze V] Title: 새로운 시작, Time: 96 ms, Memory: 13812 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,10 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("파이팅!!"); 
+            
+        System.out.print(sb);
+	}
+}
