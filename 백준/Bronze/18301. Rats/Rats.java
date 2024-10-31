130ab63e8f89c5c8b841eb5d092d6ac0f454ee3c
EoYeonjin <wks1211@gmail.com>
2024-10-31 10:56:15 +0900

    [Bronze V] Title: Rats, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n12 = Integer.parseInt(st.nextToken());
+
+        int result = (n1+1)*(n2+1)/(n12+1)-1;
+        sb.append(result);
+            
+        System.out.print(sb);
+	}
+}
