ce2dd4a88cb27f8ae99dffa13d56eb4070b8e09b
EoYeonjin <wks1211@gmail.com>
2024-10-31 14:35:17 +0900

    [Bronze V] Title: 身長 (Height), Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        
+        sb.append(B-A);
+        
+        System.out.print(sb);
+	}
+}
