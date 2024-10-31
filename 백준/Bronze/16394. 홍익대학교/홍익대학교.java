8aab5773628f90cea16663c911e7411f3b95cfcd
EoYeonjin <wks1211@gmail.com>
2024-10-31 10:13:37 +0900

    [Bronze V] Title: 홍익대학교, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int year = N - 1946;
+            
+        System.out.print(year);
+	}
+}
