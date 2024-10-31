e50da2c812ca7bdf745dfcd7cd9aafd06c161ab8
EoYeonjin <wks1211@gmail.com>
2024-10-31 10:24:28 +0900

    [Bronze V] Title: 제리와 톰, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        sb.append(B-A).append(" ").append(B);
+            
+        System.out.print(sb);
+	}
+}
