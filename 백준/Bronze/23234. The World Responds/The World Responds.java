880086800ad9819b5a5694923ff3bebf5a3e29fa
EoYeonjin <wks1211@gmail.com>
2024-10-31 14:24:46 +0900

    [Bronze V] Title: The World Responds, Time: 96 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -0,0 +1,11 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("The world says hello!");
+        
+        System.out.print(sb);
+	}
+}
