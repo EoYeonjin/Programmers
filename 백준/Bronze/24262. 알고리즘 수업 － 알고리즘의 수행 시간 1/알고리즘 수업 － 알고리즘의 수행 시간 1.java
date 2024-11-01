c70683acdc9b57386ed371f3ca991a91aecd1fb9
EoYeonjin <wks1211@gmail.com>
2024-11-01 09:20:58 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        
+        sb.append(1).append("\n").append(0);
+        
+        System.out.print(sb);
+	}
+}
