85d74d443bff8826482f92fcbab9335e0e2d8c8b
EoYeonjin <wks1211@gmail.com>
2024-11-04 11:23:46 +0900

    [Bronze V] Title: Serca, Time: 304 ms, Memory: 62960 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,23 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            sb.append(" @@@   @@@ ").append("\n");
+            sb.append("@   @ @   @").append("\n");
+            sb.append("@    @    @").append("\n");
+            sb.append("@         @").append("\n");
+            sb.append(" @       @ ").append("\n");
+            sb.append("  @     @  ").append("\n");
+            sb.append("   @   @   ").append("\n");
+            sb.append("    @ @    ").append("\n");
+            sb.append("     @     ").append("\n");
+        }
+
+        System.out.print(sb);
+	}
+}
