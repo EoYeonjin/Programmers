083e20295c8c65e7c15ef775946231703d7cf30b
EoYeonjin <wks1211@gmail.com>
2024-11-20 15:08:32 +0900

    [Bronze IV] Title: FA, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.java
@@ -0,0 +1,13 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print(fa(br.readLine()));
+    }
+
+    public static String fa(String X){
+        return "FA";
+    }
+}
