8f5ff775d14db7ee8f720b380ca269df57fce94e
EoYeonjin <wks1211@gmail.com>
2024-11-28 11:06:34 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1328 ms, Memory: 58640 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Point> points = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            points.add(new Point(x, y));            
+        }
+
+        points.sort(new Comparator<Point>(){
+            @Override
+            public int compare(Point p1, Point p2){
+                if(p1.y == p2.y){
+                    return Integer.compare(p1.x, p2.x);
+                }
+
+                return Integer.compare(p1.y, p2.y);
+            }
+        });
+
+        for (Point point : points) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+
+    static class Point{
+        int x, y;
+
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
