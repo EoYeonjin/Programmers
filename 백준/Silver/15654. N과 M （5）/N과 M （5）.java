7a4c0e0fbe6b9304b1e6d6874ca1e4b0067feaac
EoYeonjin <wks1211@gmail.com>
2024-12-02 14:38:22 +0900

    [Silver III] Title: N과 M (5), Time: 312 ms, Memory: 29024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static boolean[] used;
+    static int[] numbers;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        numbers = new int[N];
+        for (int i=0; i<N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        arr = new int[M];
+        used = new boolean[N];
+
+        backtrack(0);
+        System.out.print(sb);
+    }
+
+    public static void backtrack(int n){
+        if(n == M){
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=0; i<N; i++) {
+            if(used[i]) continue;
+
+            used[i] = true;
+            arr[n] = numbers[i];
+            
+            backtrack(n+1);
+            used[i] = false;
+        }
+    }
+}
