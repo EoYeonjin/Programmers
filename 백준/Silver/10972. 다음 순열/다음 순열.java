3f77ab2e4d2cac53daba30161ec2982bbfca08ee
EoYeonjin <wks1211@gmail.com>
2025-01-05 19:02:42 +0900

    [Silver III] Title: 다음 순열, Time: 276 ms, Memory: 18796 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static String target;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        target = br.readLine();
+        visited = new boolean[N+1];
+
+        findNextPermutation();
+    }
+
+    public static void findNextPermutation(){
+        String[] arr = target.split(" ");
+        int[] nums = new int[N];
+        
+        for (int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(arr[i]);
+        }
+
+        if(nextPermutation(nums)) {
+            for (int i=0; i<N; i++) {
+                System.out.print(nums[i] + " ");
+            }
+        }else System.out.print(-1);
+    }
+
+    public static boolean nextPermutation(int[] arr){
+        int i = arr.length - 1;
+
+        while (i>0 && arr[i-1] >= arr[i]) i--;
+        if(i == 0) return false;
+
+        int j = arr.length - 1;
+
+        while (arr[i-1] >= arr[j]) j--;
+
+        swap(arr, i-1, j);
+
+        int left = i, right = arr.length - 1;
+        while (left < right) swap(arr, left++, right--);
+        
+        return true;    
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
