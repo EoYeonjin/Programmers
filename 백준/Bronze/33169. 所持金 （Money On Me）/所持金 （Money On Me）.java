4d76d5359893a133a5e38fd68f942685da7c050f
EoYeonjin <wks1211@gmail.com>
2025-02-26 22:24:29 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.print((A*1000)+(B*10000));
+    }
+}
