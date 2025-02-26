11ee65d5ac9b50e62b586255a3e79b18f2fe4c5c
EoYeonjin <wks1211@gmail.com>
2025-02-26 22:25:36 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        if((A+B+C) <=21) System.out.print(1);
+        else System.out.print(0);
+    }
+}
