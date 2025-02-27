121b3065cbfa39bdfa28be21f3be3b4a466ec95a
EoYeonjin <wks1211@gmail.com>
2025-02-27 22:44:38 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N+1];
+
+        dp[0] = 0;
+
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=i; j++) {
+                dp[i] = Math.max(dp[i], dp[i-j] + arr[j-1]);
+            }
+        }
+
+        System.out.print(dp[N]);
+    }
+}
